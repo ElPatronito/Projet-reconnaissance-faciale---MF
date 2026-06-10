b91c5c451808cc22c49278e1a0259033ebbe6c2c
julienadam04 <julien.adam04@gmail.com>
2026-06-10 17:47:05 +0200

    Add files via upload

---
+++ build/CMakeFiles/reconnaissance.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cytech/ING1/Projet_Reconnaissance_Faciale/PCA_LDA/LDA.cpp" "CMakeFiles/reconnaissance.dir/PCA_LDA/LDA.cpp.o" "gcc" "CMakeFiles/reconnaissance.dir/PCA_LDA/LDA.cpp.o.d"
+  "/home/cytech/ING1/Projet_Reconnaissance_Faciale/PCA_LDA/PCA.cpp" "CMakeFiles/reconnaissance.dir/PCA_LDA/PCA.cpp.o" "gcc" "CMakeFiles/reconnaissance.dir/PCA_LDA/PCA.cpp.o.d"
+  "/home/cytech/ING1/Projet_Reconnaissance_Faciale/Preprocesseur/Preprocesseur.cpp" "CMakeFiles/reconnaissance.dir/Preprocesseur/Preprocesseur.cpp.o" "gcc" "CMakeFiles/reconnaissance.dir/Preprocesseur/Preprocesseur.cpp.o.d"
+  "/home/cytech/ING1/Projet_Reconnaissance_Faciale/RecognitionPipeline/RecognitionPipeline.cpp" "CMakeFiles/reconnaissance.dir/RecognitionPipeline/RecognitionPipeline.cpp.o" "gcc" "CMakeFiles/reconnaissance.dir/RecognitionPipeline/RecognitionPipeline.cpp.o.d"
+  "/home/cytech/ING1/Projet_Reconnaissance_Faciale/main_bis.cpp" "CMakeFiles/reconnaissance.dir/main_bis.cpp.o" "gcc" "CMakeFiles/reconnaissance.dir/main_bis.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/reconnaissance.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reconnaissance.dir/PCA_LDA/LDA.cpp.o"
+  "CMakeFiles/reconnaissance.dir/PCA_LDA/LDA.cpp.o.d"
+  "CMakeFiles/reconnaissance.dir/PCA_LDA/PCA.cpp.o"
+  "CMakeFiles/reconnaissance.dir/PCA_LDA/PCA.cpp.o.d"
+  "CMakeFiles/reconnaissance.dir/Preprocesseur/Preprocesseur.cpp.o"
+  "CMakeFiles/reconnaissance.dir/Preprocesseur/Preprocesseur.cpp.o.d"
+  "CMakeFiles/reconnaissance.dir/RecognitionPipeline/RecognitionPipeline.cpp.o"
+  "CMakeFiles/reconnaissance.dir/RecognitionPipeline/RecognitionPipeline.cpp.o.d"
+  "CMakeFiles/reconnaissance.dir/main_bis.cpp.o"
+  "CMakeFiles/reconnaissance.dir/main_bis.cpp.o.d"
+  "reconnaissance"
+  "reconnaissance.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reconnaissance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
